2f1f4e39703de5febb9f5c94548414878ca61702
Can US <me@canus.dev>
2023-11-18 19:18:31 +0300

    add a S7DataItemWithResponse and AGReadMultiResponse

--- multi.go
+++ multi.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 )
 
-//S7DataItem which expose as S7DataItem to use in Multiple read/write
+// S7DataItem which expose as S7DataItem to use in Multiple read/write
 type S7DataItem struct {
 	Area     int
 	WordLen  int
@@ -20,7 +20,19 @@ type S7DataItem struct {
 	Error    string
 }
 
-//implement WriteMulti
+// S7DataItem which expose as S7DataItem to use in Multiple read/write
+type S7DataItemWithResponse struct {
+	Area     int
+	WordLen  int
+	DBNumber int
+	Start    int
+	Bit      int
+	Amount   int
+	OnData   func([]byte)
+	OnError  func(string)
+}
+
+// implement WriteMulti
 func (mb *client) AGWriteMulti(dataItems []S7DataItem, itemsCount int) (err error) {
 	// Checks items
 	if itemsCount > 20 { //max variable is 20
@@ -137,7 +149,7 @@ func (mb *client) AGWriteMulti(dataItems []S7DataItem, itemsCount int) (err erro
 	return
 }
 
-//implement ReadMulti
+// implement ReadMulti
 func (mb *client) AGReadMulti(dataItems []S7DataItem, itemsCount int) (err error) {
 	// Checks items
 	if itemsCount > 20 { //max variable is 20
@@ -232,6 +244,107 @@ func (mb *client) AGReadMulti(dataItems []S7DataItem, itemsCount int) (err error
 			offset = offset + 4 + itemSize
 		} else {
 			dataItems[i].Error = ErrorText(CPUError(uint(s7ItemRead[0])))
+			offset += 4 // Skip the Item header
+		}
+	}
+
+	return
+
+}
+func (mb *client) AGReadMultiResponse(dataItems []S7DataItemWithResponse, itemsCount int) (err error) {
+	// Checks items
+	if itemsCount > 20 { //max variable is 20
+		err = fmt.Errorf(ErrorText(errCliTooManyItems))
+		return
+	}
+	s7Item := make([]byte, 12)
+	s7Multi := make([]byte, len(s7MultiReadHeaderTelegram))
+	copy(s7Multi, s7MultiReadHeaderTelegram)
+	// Fills Header
+	binary.BigEndian.PutUint16(s7Multi[13:], uint16(itemsCount*len(s7Item)+2))
+	s7Multi[18] = byte(itemsCount)
+	// Fills the Items
+	offset := 19
+	for i := 0; i < itemsCount; i++ {
+		copy(s7Item, s7MultiReadItemTelegram)
+		s7Item[3] = byte(dataItems[i].WordLen)
+		binary.BigEndian.PutUint16(s7Item[4:], uint16(dataItems[i].Amount))
+		if dataItems[i].Area == s7areadb {
+			binary.BigEndian.PutUint16(s7Item[6:], uint16(dataItems[i].DBNumber))
+		}
+		s7Item[8] = byte(dataItems[i].Area)
+
+		// Adjusts the offset
+		var addr int
+		if dataItems[i].WordLen == s7wlcounter || dataItems[i].WordLen == s7wltimer {
+			addr = dataItems[i].Start
+		} else if dataItems[i].WordLen == s7wlbit {
+			addr = dataItems[i].Start << 3
+			addr += dataItems[i].Bit // Add Bit addr
+		} else {
+			addr = dataItems[i].Start * 8
+		}
+
+		// Build the offset
+		s7Item[11] = byte(addr & 0x0FF)
+		addr = addr >> 8
+		s7Item[10] = byte(addr & 0x0FF)
+		addr = addr >> 8
+		s7Item[9] = byte(addr & 0x0FF)
+		//now expand array then put item into
+		s7Multi = append(s7Multi, s7Item...)
+		offset += len(s7Item)
+	}
+	tt, _ := interface{}(mb.transporter).(*TCPClientHandler)
+	if offset > tt.PDULength {
+		err = fmt.Errorf(ErrorText(errCliSizeOverPDU))
+		return
+	}
+	binary.BigEndian.PutUint16(s7Multi[2:], uint16(offset)) // Whole size
+	request := NewProtocolDataUnit(s7Multi)
+	//send
+	response, err := mb.send(&request)
+	if err != nil {
+		return
+	}
+	// Check ISO Length
+	resLength := len(response.Data)
+	if resLength < 22 {
+		err = fmt.Errorf(ErrorText(errIsoInvalidPDU)) // PDU too Small
+		return
+	}
+	// Check Global Operation Result
+	cpuErr := CPUError(uint(binary.BigEndian.Uint16(response.Data[17:])))
+	if cpuErr != 0 {
+		err = fmt.Errorf(ErrorText(cpuErr))
+		return
+	}
+	// Get true ItemsCount
+	itemsRead := int(response.Data[20])
+	s7ItemRead := make([]byte, 1024)
+	if itemsRead != itemsCount || itemsRead > 20 { //max var
+		err = fmt.Errorf(ErrorText(errCliInvalidPlcAnswer))
+		return
+	}
+	// Get Data
+	offset = 21
+	for i := 0; i < itemsCount; i++ {
+		// Get the Item
+		copy(s7ItemRead[0:resLength-offset], response.Data[offset:resLength])
+		if s7ItemRead[0] == 255 {
+			itemSize := int(binary.BigEndian.Uint16(s7ItemRead[2:]))
+			item1 := s7ItemRead[1]
+			if item1 != tsResOctet && item1 != tsResReal && item1 != tsResBit {
+				itemSize = itemSize >> 3
+			}
+			dataItems[i].OnData(s7ItemRead[4 : 4+itemSize])
+
+			if itemSize%2 != 0 {
+				itemSize++ // Odd size are rounded
+			}
+			offset = offset + 4 + itemSize
+		} else {
+			dataItems[i].OnError(ErrorText(CPUError(uint(s7ItemRead[0]))))
 			offset += 4 // Skip the Item header
 		}
 	}
